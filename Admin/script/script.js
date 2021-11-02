2888a99fbb4d7facf9307a736bcacff73ea15d8e
Cafe latte <83046560+Aso2001169@users.noreply.github.com>
2021-11-02 13:35:45 +0900

    11月2日授業中

---
+++ Admin/script/script.js
@@ -0,0 +1,25 @@
+new Vue({
+  el:'#app',
+  data(){
+    return{
+      menus:[
+        {
+          label:'商品追加・更新・削除',
+          path:'merchandise/merchandise.html'
+        },
+        {
+          label:'会員管理',
+          path:'merchandise/user.html'
+        },
+        {
+          label:'売上管理',
+          path:'merchandise/Earnings.html'
+        },
+        {
+          label:'商品・在庫検索',
+          path:'merchandise/contact.html'
+        }
+      ]
+    };
+  }
+})
